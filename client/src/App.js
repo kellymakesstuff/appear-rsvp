9cc2b06cc48c261b0c1375caedb36d2c8ba1f26f
Kelly Bee <kellybee@Kellys-iMac.cable.rcn.com>
2020-07-16 15:39:54 -0400

    added services files

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 export default function App() {
+  let [households, setHouseholds] = useState([])
+
+
 
 
   return <>
---
+++ client/src/services/ApiHelper.js
@@ -0,0 +1,17 @@
+import api from './apiconfig'
+
+
+export const getHouseholds = async () => {
+  const response = await api.get('/households')
+  return response.data
+}
+
+export const getGuests = async (id) => {
+  const response = await api.get(`/households/${id}/guests`)
+  return response.data
+}
+
+export const addGuest = async (id, guestInfo) => {
+  const response = await api.post(`/households/${id}/guests`, { guest: guestInfo })
+  return response.data
+}
