39c25140d90ae90eb53a352ec7b8fa996902c61b
Kelly Bee <kellybee@Kellys-iMac.cable.rcn.com>
2020-07-17 10:55:23 -0400

    moved api call to app

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,19 @@ import Main from './components/views/Main'
 
 export default function App() {
 
+  let [households, setHouseholds] = useState([])
+
+  let apiCall = async () => {
+    let householdData = await axios(api)
+    console.log(householdData)
+  }
+
+  useEffect(() => {
+    apiCall()
+  }, [])
+
   return <>
+    <h1>in App</h1>
     <Header />
     <Main />
   </>
--- client/src/components/views/Main.jsx
+++ client/src/components/views/Main.jsx
@@ -2,20 +2,11 @@ import React from 'react'
 
 export default function Main() {
 
-  let [households, setHouseholds] = useState([])
 
-  let apiCall = async () => {
-    let householdData = await axios(api)
-    console.log(householdData)
-  }
-
-  useEffect(() => {
-    apiCall()
-  }, [])
 
   return (
     <div>
-
+      <h2>in Main</h2>
     </div>
   )
 }
