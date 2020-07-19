5b88b9e053ca0657be66084cb651a0230dcaff3c
Kelly Bee <kellybee@Kellys-iMac.cable.rcn.com>
2020-07-19 18:01:58 -0400

    added new photo

--- client/src/components/functional/AddPhoto.jsx
+++ client/src/components/functional/AddPhoto.jsx
@@ -9,20 +9,26 @@ export default function AddPhoto({ banana, photos, setPhotos }) {
   console.log("photos in addphoto", photos)
 
   let parsedBanana = parseInt(banana)
+  let existing = photos.filter(household => household.household_id === parsedBanana)
+  console.log("existing", existing)
 
   let [newPhoto, setNewPhoto] = useState({
+    existing,
     src: null,
     height: 4,
     width: 3,
     household_id: parsedBanana
   })
 
+  console.log("newPhoto", newPhoto)
+
   let handleNewPhoto = (e) => {
-    let imgUrl = `"${e}"`
+    let imgUrl = e
     let heightNum = 4
     let widthNum = 3
 
     setNewPhoto({
+      existing,
       src: imgUrl,
       height: heightNum,
       width: widthNum,
@@ -37,6 +43,10 @@ export default function AddPhoto({ banana, photos, setPhotos }) {
     addPhoto(newPhoto)
   }
 
+
+
+
+
   return <>
     <div>
       <h1>photosss</h1>
@@ -44,9 +54,11 @@ export default function AddPhoto({ banana, photos, setPhotos }) {
         <input type="text" onChange={(e) => handleNewPhoto(e.target.value)} />
       </form>
       <button onClick={() => submitNewPhoto()}>add photo</button>
-      <Link to="/">Back to Gallery</Link>
+      <Link to="/" refresh="true">Back to Gallery</Link>
 
       {newPhoto.src && <img src={newPhoto.src} />}
+
+      <Gallery photos={existing} />
 
     </div>
   </>
