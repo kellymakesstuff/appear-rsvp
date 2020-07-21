b469cd89c50ca9c9dbab5b8ee95bca8126413ad5
Kelly Bee <kellybee@Kellys-iMac.cable.rcn.com>
2020-07-21 12:40:49 -0400

    fixed async issue with allGuests not lining up when NOT from local storage (works both ways now)

--- client/src/components/views/AccessCode.jsx
+++ client/src/components/views/AccessCode.jsx
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import { Redirect } from 'react-router-dom'
+import axios from 'axios'
 
 export default function AccessCode({
   households, setHouseholds,
   banana, setBanana,
   toMain, setToMain,
-  currentHouse, setCurrentHouse }) {
+  currentHouse, setCurrentHouse,
+  allGuests, setAllGuests }) {
 
   console.log("banana", banana, setCurrentHouse)
 
@@ -15,11 +17,23 @@ export default function AccessCode({
     localStorage.setItem("banana", code)
   }
 
+  let allGuestCall = async () => {
+    let guestData = await axios(`https://salty-taiga-76954.herokuapp.com/households/${banana}/guests`)
+    setAllGuests(guestData.data)
+
+  }
+
+  let handleGuests = () => {
+    setCurrentHouse(households[banana - 1])
+    allGuestCall()
+    setToMain(true)
+  }
+
   return <>
     {toMain ? <Redirect to="/household" /> : null}
     <div>
       <h1>access code</h1>
-      <form onSubmit={() => { setCurrentHouse(households[banana - 1]); setToMain(true) }}>
+      <form onSubmit={() => handleGuests()}>
         <input type="text" onChange={e => handleBanana(e)} />
         <input type="submit" />
       </form>
